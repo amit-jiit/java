701a3cb8a7299f2fcd330b4aaf94983476090924
Amit Kumar <amit2016jiit@gmail.com>
2020-09-27 20:39:02 +0530

    Add files via upload

--- mongodb/test/connection.js
+++ mongodb/test/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+//ES6 Promises
+mongoose.Promise = global.Promise;
+//connect to the database before test run
+// this will run once
+before(function(done){
 // connect to mongodb localhost testaroo database
 mongoose.connect('mongodb://localhost/testaroo');
 
@@ -7,6 +12,16 @@ mongoose.connect('mongodb://localhost/testaroo');
 //'on' keep listening all the event
 mongoose.connection.once('open',function(){
 console.log('connection has been made, now make fireworks');
+done();
 }).on('error',function(error){
     console.log('connection error:',error);
+});
+});
+//drop the characters collection before each test
+// this will run before every single test
+beforeEach(function(done){
+// Drop thecollection
+mongoose.connection.collections.mariochars.drop(function(){
+done();
+});
 });
